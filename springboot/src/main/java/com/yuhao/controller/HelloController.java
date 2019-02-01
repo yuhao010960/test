532ce6aeb270a3c4a078ac6d6f9a2acc17659471
yuhao <18701550200@163.com>
2019-02-01 16:42:07 +0800

    ceshi

--- springboot/src/main/java/com/yuhao/controller/HelloController.java
+++ springboot/src/main/java/com/yuhao/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.yuhao.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class HelloController {
 	@RequestMapping("/juran")
 	public String yuhao() {
 		return "浩浩：我爱你居然";
+	}
+
+	@RequestMapping("yuhao/{name}")
+	public String yyy(@PathVariable String name){
+		return name;
 	}
 }
